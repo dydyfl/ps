386684045ea87ba656532e71f8c3f2fcf67c1edc
dydyfl <32984097+dydyfl@users.noreply.github.com>
2018-05-26 14:01:03 +0300

    Add files via upload

---
+++ Simulare/Simulare/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Simulare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(1122, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "S0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(1122, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "S1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(1122, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "S2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(1122, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 43);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "S3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(1122, 212);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "S4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(1122, 258);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 41);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "S5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(1111, 331);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(47, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "S6";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(1183, 331);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(47, 21);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "S7";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(1250, 331);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(47, 21);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "S8";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(183, 319);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(786, 319);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(183, 639);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(347, 639);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(54, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(1115, 526);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 38);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "ALARMA";
+            this.label1.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(46, 22);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "START";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(259, 22);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(413, 20);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "IP:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Port:";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(283, 54);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Connect";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1327, 728);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Simulare/Simulare/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCPLibrary;
+
+namespace Simulare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+             this.FormClosing += Form1_FormClosing;
+             Timer timer = new Timer();
+             timer.Interval = 100;
+             timer.Tick += timer_Tick;
+           
+             timer.Start();
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (alarmState == 0)
+            {
+                label1.Visible = false;
+                count = 0;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                checkBox3.Enabled = true;
+            }
+            else
+            {
+                label1.Visible = true;
+                count++;
+                if(count==50)
+                    alarmState = 0;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false;
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client.Close();
+        }
+
+
+        int alarmState = 0;
+        int count = 0;
+        byte[] buffer_send = new byte[4];
+        byte[] buffer_receive = new byte[4];
+        TcpClient client = null;
+
+        public byte set_bit(byte b, int position)
+        {
+
+            return b |= (byte)(1 << position);
+        }
+
+        public byte clear_bit(byte b, int position)
+        {
+
+            return b &= ((byte)~(1 << position));
+        }
+        public bool get_bit(byte b, int position)
+        {
+            if (((b >> position) & 1)==1)
+                return true;
+            else
+                return false;
+        }
+
+       
+        public void SendToServer()
+        {
+            TCP_API.send(client, buffer_send);
+        }
+
+        public void ReceiveFromServer()
+        {
+            TCP_API.receive(client, ref buffer_receive);
+            Console.WriteLine(buffer_receive[0]);
+            Console.WriteLine(get_bit(buffer_receive[0], 6));
+            
+            Console.WriteLine(get_bit(buffer_receive[0], 5));
+            
+            Console.WriteLine(get_bit(buffer_receive[0], 4));
+            
+            Console.WriteLine(get_bit(buffer_receive[0], 3));
+            if (get_bit(buffer_receive[0], 6))
+                textBox1.BackColor = Color.Green;
+            else
+                textBox1.BackColor = Color.Red;
+
+            if (get_bit(buffer_receive[0], 5))
+                textBox2.BackColor = Color.Green;
+            else
+                textBox2.BackColor = Color.Red;
+
+            if (get_bit(buffer_receive[0], 4))
+                textBox3.BackColor = Color.Green;
+            else
+                textBox3.BackColor = Color.Red;
+
+            if (get_bit(buffer_receive[0], 3))
+                textBox4.BackColor = Color.Green;
+            else
+                textBox4.BackColor = Color.Red;
+
+
+            if (get_bit(buffer_receive[0], 7))
+            {
+                alarmState = 1;
+            }
+        }
+
+        
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            textBox1.BackColor = Color.Red;
+            textBox2.BackColor = Color.Red;
+            textBox3.BackColor = Color.Red;
+            textBox4.BackColor = Color.Red;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+            button2.Text = "S1- OFF";
+            button3.Text = "S2- OFF";
+            button4.Text = "S3- OFF";
+            button5.Text = "S4- OFF";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            buffer_send[0] = set_bit(buffer_send[0], 7);
+            SendToServer();
+            ReceiveFromServer();
+            buffer_send[0] = clear_bit(buffer_send[0], 7);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "S1- OFF")
+            {
+                button2.Text = "S1- ON";
+                buffer_send[0] = set_bit(buffer_send[0], 6);
+            }
+            else if (button2.Text == "S1- ON")
+            {
+                button2.Text = "S1- OFF";
+                buffer_send[0] = clear_bit(buffer_send[0], 6);
+            }
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (button3.Text == "S2- OFF")
+            {
+                button3.Text = "S2- ON";
+                buffer_send[0] = set_bit(buffer_send[0], 5);
+            }
+            else if (button3.Text == "S2- ON")
+            {
+                button3.Text = "S2- OFF";
+                buffer_send[0] = clear_bit(buffer_send[0], 5);
+            }
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "S3- OFF")
+            {
+                button4.Text = "S3- ON";
+                buffer_send[0] = set_bit(buffer_send[0], 4);
+            }
+            else if (button4.Text == "S3- ON")
+            {
+                button4.Text = "S3- OFF";
+                buffer_send[0] = clear_bit(buffer_send[0], 4);
+            }
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "S4- OFF")
+            {
+                button5.Text = "S4- ON";
+                buffer_send[0] = set_bit(buffer_send[0], 3);
+            }
+            else if (button5.Text == "S4- ON")
+            {
+                button5.Text = "S4- OFF";
+                buffer_send[0] = clear_bit(buffer_send[0], 3);
+            }
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            buffer_send[0] = set_bit(buffer_send[0], 2);
+            SendToServer();
+            ReceiveFromServer();
+            buffer_send[0] = clear_bit(buffer_send[0], 2);
+        }
+
+        
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked==true)
+                buffer_send[0] = set_bit(buffer_send[0], 1);
+            else
+                buffer_send[0] = clear_bit(buffer_send[0], 1);
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+                buffer_send[0] = set_bit(buffer_send[0], 0);
+            else
+                buffer_send[0] = clear_bit(buffer_send[0], 0);
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+                buffer_send[1] = set_bit(buffer_send[1], 7);
+            else
+                buffer_send[1] = clear_bit(buffer_send[1], 7);
+            SendToServer();
+            ReceiveFromServer();
+        }
+
+        private void button8_Click(object sender, EventArgs e1)
+        {
+            String server = textBox5.Text;
+            int port = Int32.Parse(textBox6.Text);
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+
+                client = new TcpClient(server, port);
+
+
+
+                // Close everything.
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            button8.Enabled = false;
+
+        }
+
+        
+    }
+}
